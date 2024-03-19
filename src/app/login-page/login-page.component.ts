018f3f12f3a161772aef97e73d17447628abd117
sri-tarun-indugula <indugulasritharun123@gmail.com>
2024-03-19 10:03:02 +0530

    created a login component

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { MainContactService } from '../main-contact.service';
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
@@ -9,10 +10,13 @@ export class LoginPageComponent {
   email = new FormControl('', [Validators.required, Validators.email]);
   password = new FormControl('', [Validators.required]);
   hide = true;
+  loginConformation=false;
 
-  constructor() {
-    
+  constructor(private maincontact: MainContactService) {
+
   }
+
+
 
   getErrorMessage(type: any) {
     if (type == 'email') {
@@ -28,5 +32,13 @@ export class LoginPageComponent {
       return '';
     }
     return ''
+  }
+
+  login(){
+    if(!this.email.invalid&&!this.password.invalid){
+      const loginData={email:this.email.value,password:this.password.value}
+      this.loginConformation=this.maincontact.loginValidator(loginData)
+    }
+    console.log(this.loginConformation)
   }
 }
