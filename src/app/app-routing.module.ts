cde3d5fe0be79dd25f3791d80760b4dbcac9342a
sri-tarun-indugula <indugulasritharun123@gmail.com>
2024-03-19 10:01:15 +0530

    added rouite to the project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { CardContactsManagerComponent } from './card-contacts-manager/card-contacts-manager.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginPageComponent },
+  { path: '', component: CardContactsManagerComponent },
+  {path:'cardContact',component:CardContactsManagerComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
