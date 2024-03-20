277cd18bf9eefaa17e9ccaa81a0787cb14020ba0
sri-tarun-indugula <indugulasritharun123@gmail.com>
2024-03-20 10:44:32 +0530

    added routeing of contactmanager table

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { CardContactsManagerComponent } from './card-contacts-manager/card-contacts-manager.component';
+import { ContactManagerTableComponent } from './contact-manager-table/contact-manager-table.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginPageComponent },
   { path: '', component: CardContactsManagerComponent },
-  {path:'cardContact',component:CardContactsManagerComponent}
+  {path:'cardContact',component:CardContactsManagerComponent},
+  {path:'contact-list',component:ContactManagerTableComponent}
 ];
 
 @NgModule({
